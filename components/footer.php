0b863875e05d5ebadd0248fdd06030028c912898
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2025-06-01 22:05:41 +0000

    chatbot added

--- components/footer.php
+++ components/footer.php
@@ -35,7 +35,7 @@ src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.j
 integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
 crossorigin="anonymous"></script>
 
-<?php include './assets/chatbot/chatbot.php'; ?>
+<elevenlabs-convai agent-id="agent_01jwkj6gv2e26byj94crwgjt87"></elevenlabs-convai><script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
 </body>
 
 </html>
